20613aaf6d3d5c50421c8ccd9102003e0b3233cf
조민혁 <whwhgurdl@gmail.com>
2024-10-16 20:48:07 +0900

    가장 많이 받은 선물

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Programmers
+{
+
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            most_received_gift test = new most_received_gift();
+
+            test.solution(["joy", "brad", "alessandro", "conan", "david"], ["alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"]);
+        }
+    }
+}
